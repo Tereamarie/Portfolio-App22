4510c489ecfe2e67913bf5b3acd787c620856783
Marie Knowlton <mtknowlton67@gmail.com>
2022-06-11 15:28:50 -0700

    Added images to projects

--- src/utils/project-list.js
+++ src/utils/project-list.js
@@ -1,11 +1,11 @@
 let project_list = [
 	{
 		image:
-			'https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+			'https://wallpapercave.com/dwp1x/wp2171387.jpg',
 		title: 'Intro-CSS',
 		des:
-			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-		tags: [ 'HTML', 'CSS', 'Flexbox', 'React', 'JavaScript' ],
+			"A simple App using HTML / CSS / In-line styling linked CSS stylesheet to the index.html. Implemented CSS reset. Marked up html semanticaly. Changed all fonts and styles according to documentation.",
+		tags: [ 'HTML', 'CSS', 'in-line-block', ],
 		links: {
 			launch: 'https://yoparkguidehtmlcss.netlify.app',
 			github: 'https://github.com/Tereamarie/Intro-CSS'
@@ -25,26 +25,26 @@ let project_list = [
 	},
 	{
 		image:
-			'https://images.unsplash.com/photo-1553034545-32d4cd2168f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-		title: 'Project 3',
+			'https://cdn11.bigcommerce.com/s-odybbbawa7/images/stencil/1280x1280/products/4503/6896/varsity-2246-8-wall-mounted-scoreboard-19__04916.1633630110.jpg?c=1',
+		title: 'React Score Board',
 		des:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-		tags: [ 'CSS', 'Flexbox', 'Grid', 'React', 'JavaScript' ],
+		tags: [ 'CSS', 'Flexbox', 'React', 'JavaScript' ],
 		links: {
-			launch: '#',
-			github: 'https://www.github.com/Dhaval1403/React-Portfolio-Template'
+			launch: 'https://reactapp-scoreboard.netlify.app',
+			github: 'https://reactapp-scoreboard.netlify.app'
 		}
 	},
 	{
 		image:
-			'https://images.unsplash.com/photo-1491975474562-1f4e30bc9468?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-		title: 'Project 4',
+			" ./widget1.png ",
+		title: 'React Stateful Widget',
 		des:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		tags: [ 'React', 'Redux', 'Node.js', 'HTML', 'CSS', 'JavaScript' ],
 		links: {
-			launch: '#',
-			github: 'https://www.github.com/Dhaval1403/React-Portfolio-Template'
+			launch: 'https://react-stateful-widget.netlify.app',
+			github: 'https://github.com/Teresa-code-123/react-stateful-widgets'
 		}
 	}
 ];
